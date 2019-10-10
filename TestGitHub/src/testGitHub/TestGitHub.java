74c5f006dc096752b1e753ce562f0125782068da
user <user@LDW>
2019-10-10 15:38:19 +0300

    Создали дополнительную ветку для треугольника. Созадли класс треугольник. Инициализировал треугольник и  вывел данные.

--- TestGitHub/src/testGitHub/TestGitHub.java
+++ TestGitHub/src/testGitHub/TestGitHub.java
@@ -19,15 +19,37 @@ class Line {
 	}
 }
 
+class Triangle {
+	Point a;
+	Point b;
+	Point c;
+	
+	Triangle(Point valpoint1, Point valpoint2, Point valpoint3) {
+		a = valpoint1;
+		b = valpoint2;
+		c = valpoint3;
+	}
+}
+
 public class TestGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello Big World!");
+		//for line
 		Point a = new Point(1, 2);
 		Point b = new Point(3, 4);		
 		Line ab = new Line(a, b);
-		System.out.println("Line AB:\nA(" + ab.a.x + ", " + ab.a.y + ")\nB(" + ab.b.x + ", " + ab.b.y + ")");
+		System.out.println("Line AB:");
+		System.out.println("A(" + ab.a.x + "," + ab.a.y + ")");
+		System.out.println("B(" + ab.b.x + ", " + ab.b.y + ")");
+		//for triangle
+		Point c = new Point(5, 6);
+		Triangle abc = new Triangle(a, b, c);
+		System.out.println("Triangle ABC:");
+		System.out.println("A(" + abc.a.x + "," + abc.a.y + ")");
+		System.out.println("B(" + abc.b.x + "," + abc.b.y + ")");
+		System.out.println("C(" + abc.c.x + "," + abc.c.y + ")");
 				
 	}
 
